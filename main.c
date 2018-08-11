4bbc6c18cbefde9bc520336c956aac89c5c57136
deeptisl <41547887+deeptisl@users.noreply.github.com>
2018-08-11 22:39:42 +0530

    Whitespace

--- main.c
+++ main.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
+#include<string.h>
 int main()
 {
     char s[100],s1[100];
-    int i,j,len,len1;
-    printf("enter the 1st string:");
+    printf("enter the string:");
     gets(s);
-    printf("enter the second string:");
-    gets(s1);
-    len=strlen(s);
-    len1=strlen(s1);
-    if(len==len1){
-        for(i=0;i<len;i++){
-            for(j=0;j<len1;j++){
-                if(s[j]==s[i])
-                    break;
-            }
-        }
-        printf("the string is anagram\n");
-        printf("%s    %s ",s1,s);
+    int i;
+    int len=strlen(s);
+    for(i=0;i<len;i++){
+            if(s[i]==' ')
+        s1[i]=s[i+1];
     }
-    else
-    {
-        printf("string are not anagram");
-    }
+    s1[len]='\0';
+    puts(s1);
 }
+
